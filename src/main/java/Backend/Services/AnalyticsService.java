f24a1e9f66056354f1b3ddfecf71cbc76d6eef99
Ibra1221 <ibrahimsalem6622005@gmail.com>
2025-11-23 02:50:18 +0200

    ADD: Added lessons average calculations

--- src/main/java/Backend/Services/AnalyticsService.java
+++ src/main/java/Backend/Services/AnalyticsService.java
@@ -23,12 +23,10 @@ public class AnalyticsService {
     
     public ArrayList<HashMap<String, Integer>> getStudentsProgress(){
     ArrayList<HashMap<String, Integer>> totalProgress = new ArrayList<HashMap<String,Integer>>();
-    ArrayList<Integer> enrolledStudentsIds = course.getStudentIds();
-    int numberOfEnrolledStudents = course.getStudentIds().size();
-    CourseService courseService = new CourseService(course);
-    for(int i = 0; i < numberOfEnrolledStudents; i++){
+    ArrayList<Student> students = getEnrolledStudents();
+    for(int i = 0; i < students.size(); i++){
         HashMap<String, Integer> studentProgress = new HashMap<String, Integer>();
-        Student s = courseService.getStudentById(i);
+        Student s = students.get(i);
         String name = s.getUsername();
         int progress = calculateCourseProgress(s);
         studentProgress.put(name, progress);
@@ -43,7 +41,42 @@ public class AnalyticsService {
         return progress;
     }
     
+    public double getAllLessonsAverage(){
+        double sum = 0;
+        ArrayList<Student> enrolledStudents = getEnrolledStudents();
+        ArrayList<Lesson> totalLessons = course.getLessons();
+        for(int i = 0; i < totalLessons.size(); i++){
+            sum += getLessonAverage(totalLessons.get(i).getLessonId());
+        }
+        double average = sum / enrolledStudents.size();        
+        return average;
+    }
     
+    public double getLessonAverage(int lessonId){
+        double sum = 0.0;
+        ArrayList<Student> enrolledStudents = getEnrolledStudents();
+        for(int i = 0; i < enrolledStudents.size(); i++){
+            if(enrolledStudents.get(i).getCompletedLessonsByCourseId(course.getCourseId()).contains(lessonId)){
+                sum += enrolledStudents.get(i).getQuizScore(course.getCourseId(), lessonId);
+            }
+            else {
+                sum += 0;
+            }
+        }
+        double average = sum / enrolledStudents.size();        
+        return average;
+    }
+    
+    private ArrayList<Student> getEnrolledStudents(){
+        ArrayList<Integer> studentsIds = course.getStudentIds();
+        int numberOfEnrolledStudents = studentsIds.size();
+        ArrayList<Student> students = new ArrayList<Student>();
+        CourseService courseService = new CourseService(course);
+        for(int i = 0; i < numberOfEnrolledStudents; i++){
+        students.add(courseService.getStudentById(studentsIds.get(i)));
+        }
+        return students;
+    }
     
 } 
     
